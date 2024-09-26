01a5c192d4e5350b20f407517e67f222898ffb1b
esteban1324 <estebanlinarez@ymail.com>
2024-09-26 15:41:26 -0400

    CNN forward pass made.

--- HW2/CNNclassify.py
+++ HW2/CNNclassify.py
@@ -44,7 +44,7 @@ class CNN(nn.Module):
         self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv(64, 128, 3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
-        self.conv4 = nn.Conv(128, 256, stride=1, padding=1)
+        self.conv4 = nn.Conv(128, 256, 3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(2, stride=2)
         self.fc1 = nn.Linear(256 * 2 * 2, 128)
         self.fc2 = nn.Linear(128, 64)
@@ -54,14 +54,15 @@ class CNN(nn.Module):
     # forward pass
     def forward(self, x):
         x = x.to(device)
-       
-
-
-
-
-
-
-
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.bn2(self.conv2(x))))
+        x = self.pool(self.relu(self.bn3(self.conv3(x))))
+        x = self.pool(self.relu(self.conv4(x)))
+        #x = x.view(-1, 256 * 2 * 2)
+        x = x.view(x.size(0), -1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.out(x)
         return x
 
 net = CNN()
